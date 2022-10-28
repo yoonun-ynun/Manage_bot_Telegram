483d2202c0a6b522bd1b58848e5cff8c4b0d750b
yoonun <yoontkyoonun@gmail.com>
2022-10-28 12:35:44 +0900

    hitomi update

--- src/gethitomi.java
+++ src/gethitomi.java
@@ -8,31 +8,36 @@ import java.net.URL;
 public class gethitomi {
     static int count = 0;
     void getimage(String key) throws Exception{
+        //이미지를 가져올 주소
         String URL = "https://hitomi.la/reader/" + key + ".html#1";
 
+        //이미지의 해시값을 가져옴
         URL url = new URL("https://ltn.hitomi.la/galleries/" + key + ".js");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
         StringBuilder sb = new StringBuilder();
         String line;
-        while((line = br.readLine()) != null){
+        while((line = br.readLine()) != null) {
             sb.append(line);
         }
         JSONObject info = new JSONObject(sb.toString().substring(18));
-
         String hash = info.getJSONArray("files").getJSONObject(0).getString("hash");
-        String postfix = hash.substring(hash.length()-3);
 
+        //중간에 들어갈 주소 구하기 hash의 마지막 글자 3개를 2 0 1 순으로 배치한 것을 16진수로 취급하고 10진수로 변환
+        String postfix = hash.substring(hash.length()-3);
         int secret = Integer.parseInt(new StringBuilder().append(postfix.charAt(2)).append(postfix.charAt(0)).append(postfix.charAt(1)).toString(), 16);
 
+        //gethitomisub에서 중간에 들어갈 주소가 a 인지 b인지 구함 참고: https://ltn.hitomi.la/gg.js
         String hitomiurl = "https://" + new gethitomisub().check(secret) + "a.hitomi.la/webp/1666922401/" + secret +"/" +  hash + ".webp" ;
 
         url = new URL(hitomiurl);
         con = (HttpURLConnection) url.openConnection();
 
+        //헤더에 리퍼러를 꼭 reader 주소로 해야함 리퍼러 설정하지 않을 시 403 리턴
         con.setRequestProperty("Referer", URL);
         con.setRequestMethod("GET");
 
+        //이미지 저장
         InputStream is = con.getInputStream();
         FileOutputStream outputStream = new FileOutputStream(new File("your_path","hitomi0" + count + ".png"));
 
